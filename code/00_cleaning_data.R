45f35d73999835150df10d1faaab0506443cab33
Marcos Angelini <angelini75@gmail.com>
2022-01-26 17:28:28 -0300

    .

--- code/00_cleaning_data.R
+++ code/00_cleaning_data.R
@@ -1,7 +1,13 @@
-library(aqp)
+# install.packages("tidyverse")
+# install.packages("readxl")
+# install.packages("parzer")
+
 library(tidyverse)
 library(readxl)
 rm(list = ls())
+
+setwd("C:/Users/angel/git/NationalSoilDB/") # set your working directory
+
 
 # Pipe symbol %>% (in tidyverse package) --------------------------------------
 # We use the pipe symbol %>%  to concatenate actions (functions)
@@ -21,6 +27,7 @@ hor_phys <- read_excel("data_input/Profiles_data.xlsx", sheet = "Hor_phys")
 hor_mech <- read_excel("data_input/Profiles_data.xlsx", sheet = "Hor_mech")
 # metadata (not available)
 
+
 # Clean data and select useful variables =======================================
 # Site -------------------------------------------------------------------------
 site %>% View()
@@ -30,23 +37,32 @@ site <- site %>%
 #  How many NAs per column there are?
 is.na(site) %>% colSums()
 # Select (and rename) useful columns and filter out usefulness rows
+names(site)
 site <- site %>% 
   select(pid = ProfID, x = X_coord, y = Y_coord, year = Year, kod_t) %>% # select and rename
   filter(!is.na(x)) # we remove profiles without X coordinate
 site
 summary(site)
 
-# Q1. complete the following code to select only the column year
+# In case that you coordinate system is in degrees, minutes and seconds (42°34’18.7 N) 
+# or similar format, parzer package can conver them to decimal degrees
+# https://github.com/ropensci/parzer
+library(parzer)
+messy_coordinates <- 
+  c("45N54.2356", "45.98739874", "40°34.75’N", "42°30’ 00.725 N") # your column of latitude
+parse_lat(messy_coordinates)
+
+
+# Check the distribution of the sample time
 
 site %>% 
-  select() %>% # complete this line
-  ggplot(aes(year)) + geom_histogram()
+  select(year) %>% # complete this line
+  ggplot(aes(x=year)) + geom_histogram(binwidth = 1)
 
 # remove duplicated
 site <- unique(site)
 
 
-#
 # Horizon description ----------------------------------------------------------
 hor_des %>% View()
 # Remove first row 
@@ -72,10 +88,6 @@ hor_des <- hor_des %>%
   mutate(hor_no = as.numeric(hor_no),
          top = as.numeric(top),
          bottom = as.numeric(bottom))
-
-# Q2. Select only top horizons
-hor_des %>% 
-  filter()
 
 # Horizon chemical analysis ----------------------------------------------------------
 hor_chem
@@ -127,6 +139,7 @@ hor_phys <- hor_phys %>%
 is.na(hor_phys) %>% colSums()
 
 # Select (and rename) useful columns and filter out usefulness rows
+names(hor_phys)
 hor_phys <- hor_phys %>% 
   select(hid = HorID, bd = Bulk_density, Real_density, h_water = Higroscopic_water, 
          porosity = Porosity, wp = Wilting_point, fc = Field_capacity,
@@ -195,123 +208,3 @@ hor <- hor_des %>%
 write_csv(site, "data_output/site.csv")
 write_csv(hor, "data_output/horizon.csv")
 
-################################################################################
-## QUALITY CHECKS ##############################################################
-################################################################################
-library(aqp)
-library(sf)
-library(sp)
-library(mapview)
-
-# Load site and horizon data ---------------------------------------------------
-site <- read_csv("data_output/site.csv")
-hor <-  read_csv("data_output/horizon.csv")
-
-# Check locations ----------------------------------------------------- 
-# https://epsg.io/6204
-site %>% 
-  st_as_sf(coords = c("x", "y"), crs = 6204) %>% # convert to spatial object
-  mapview() # visualise in an interactive map
-
-# repeated locations
-x <- site %>% 
-  st_as_sf(coords = c("x", "y"), crs = 6204)  
-# evaluate which sites are at zero distance
-sp::zerodist(as_Spatial(x))
-
-# identify the profiles
-x <- x[sp::zerodist(as_Spatial(x)) %>% as.vector(),] %>% 
-  st_drop_geometry()
-
-# List of pid
-x$pid[x$pid %>% order()]
-unique(x$pid)
-
-# Convert data into a Soil Profile Collection ----------------------------------
-depths(hor) <- pid ~ top + bottom
-site(hor) <- left_join(site(hor), site)
-profiles <- hor
-
-profiles
-
-# aqp::coordinates(x) <- ~x+y
-# aqp::proj4string(x) <- "+proj=tmerc +lat_0=0 +lon_0=21 +k=0.9999 +x_0=500000 +y_0=0 +ellps=bessel +towgs84=682,-203,480,0,0,0,0 +units=m +no_defs "
-
-# plot first 20 profiles using pH as color
-plotSPC(x = profiles[1:20], name = "hor_mk", color = "ph_h2o")
-
-# check data integrity
-# A valid profile is TRUE if all of the following criteria are false:
-#    + depthLogic : boolean, errors related to depth logic
-#    + sameDepth : boolean, errors related to same top/bottom depths
-#    + missingDepth : boolean, NA in top / bottom depths
-#    + overlapOrGap : boolean, gaps or overlap in adjacent horizons
-aqp::checkHzDepthLogic(profiles)
-# get only non valid profiles
-aqp::checkHzDepthLogic(profiles ) %>% 
-  filter(valid == FALSE) 
-# visualize some of these profiles by the pid
-subset(profiles, grepl("P0142", pid, ignore.case = TRUE))
-subset(profiles, grepl("P0494", pid, ignore.case = TRUE))
-subset(profiles, grepl("P3847", pid, ignore.case = TRUE))
-
-
-# keep only valid profiles -----------------------------------------------------
-clean_prof <- HzDepthLogicSubset(profiles)
-metadata(clean_prof)$removed.profiles
-
-# Save clean data --------------------------------------------------------------
-# first, we save the soilProfileCollection object
-saveRDS(clean_prof, file = "data_output/profiles.RData")
-# now, split the SPC to have horizon data and site data 
-write_csv(clean_prof@horizons, "clean_horizons.csv")
-write_csv(clean_prof@site, "clean_site.csv")
-
-# graphical inspections & descriptive statistics ===============================
-s <- aqp::slab(clean_prof, 
-               fm = ~ clay + sand + ph_h2o + humus,
-               slab.structure = 0:100,
-               slab.fun = function(x) quantile(x, c(0.01, 0.5, 0.99), na.rm = TRUE))
-
-ggplot(s, aes(x = top, y = X50.)) +
-  # plot median
-  geom_line() +
-  # plot 10th & 90th quantiles
-  geom_ribbon(aes(ymin = X1., ymax = X99., x = top), alpha = 0.2) +
-  # invert depths
-  xlim(c(100, 0)) +
-  # flip axis
-  coord_flip() +
-  facet_wrap(~ variable, scales = "free_x")
-
-# Names
-names(clean_prof@horizons)
-
-# function to detect outliers
-is_outlier <- function(x) {
-  return(x < quantile(x, probs = 0.05, na.rm = TRUE) - 1.5 * IQR(x, na.rm = TRUE) |
-           x > quantile(x, probs = 0.95, na.rm = TRUE) + 1.5 * IQR(x, na.rm = TRUE))
-}
-
-# identify outliers for all soil properties
-clean_prof@horizons %>%
-  mutate_at(.vars = 9:34,.funs = function(y) ifelse(is_outlier(y), y, as.numeric(NA))) 
-  
-# create a table to review suspicious values (pay attention to the steps)
-
-# 1. select horizon table from clean_prof
-suspicious <- clean_prof@horizons %>% 
-  # 2. keep only outliers for each soil property (columns 9 to 34)
-  mutate_at(.vars = 9:34,.funs = function(y) ifelse(is_outlier(y), y, as.numeric(NA))) %>% 
-  # 3. define key columns (profil id and horizon id)
-  group_by(pid, hid) %>% 
-  # 4. select soil properties (from:to)
-  select(caco3:sand) %>% 
-  # 5. pivot table from wide to long
-  pivot_longer(cols = caco3:sand) %>% 
-  # 6. remove missing values (NAs)
-  na.omit()
-
-suspicious
-
-write_csv(suspicious, "data_output/suspicious.csv")
