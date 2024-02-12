f63dc176457e96ca8f978032481b8911a7591cf6
Luis Rodriguez-Lado <luis.r.lado@gmail.com>
2024-02-12 18:06:18 +0100

    Update data2sql.R

--- code/data2sql.R
+++ code/data2sql.R
@@ -2,184 +2,51 @@
 # Then Populate the SQL tables with data stores in dataframes
 
 library('RPostgreSQL')
+library('dplyr')
 
 dsn_database = "carsis"   # Specify the name of your Database
 dsn_hostname = "localhost"  
-dsn_port = "5432"                # Specify your port number. e.g. 98939
-dsn_uid = "user"         # Specify your username. e.g. "admin"
-dsn_pwd = ""        # Specify your password. e.g. "xxx"
+dsn_port = "5432"                # Specify your port number
+dsn_uid = "luislado"         # Specify your username. e.g. "admin"
+dsn_pwd = ""        # Specify your password
 
 tryCatch({
   drv <- dbDriver("PostgreSQL")
-  print("Connecting to Database…")
-  conn <- dbConnect(drv, 
+  print("connecting to Database…")
+  con <- dbConnect(drv, 
                       dbname = dsn_database,
                       host = dsn_hostname, 
                       port = dsn_port,
                       user = dsn_uid, 
                       password = dsn_pwd)
-  print("Database Connected!")
+  print("Database conected!")
 },
 error=function(cond) {
-  print("Unable to connect to Database.")
+  print("Unable to conect to Database.")
 })
-
-# Add POSTGIS
-dbSendQuery(conn, "CREATE EXTENSION IF NOT EXISTS postgis;")
-
-# Fetch data
-df <- dbGetQuery(conn, "SELECT * FROM plot");df # Los datos los he añadido con el tibble de más abajo
-
 
 
 ## SQL CODE TO CREATE TABLES----
-## Table:  project
-create_table_query <- "
-CREATE TABLE IF NOT EXISTS project (
-  project_id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  description TEXT
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
+source("code/create_tables.R")
 
-## Table:  site
-create_table_query <- " CREATE TABLE IF NOT EXISTS site (
-  site_id SERIAL PRIMARY KEY,
-  site_code VARCHAR(255),
-  location GEOGRAPHY(Point)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
 
-## Table:  plot
-create_table_query <- " CREATE TABLE IF NOT EXISTS plot (
-  plot_id SERIAL PRIMARY KEY,
-  plot_code VARCHAR(255),
-  site_id INTEGER NOT NULL,
-  plot_type VARCHAR(255),
-  FOREIGN KEY (site_id) REFERENCES site(site_id)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
+## Fetch data
+project.df <- dbGetQuery(con, "SELECT * FROM project"); project.df
+site.df <- dbGetQuery(con, "SELECT * FROM site"); site.df # Los datos los he añadido con el tibble de más abajo
+plot.df <- dbGetQuery(con, "SELECT * FROM plot"); plot.df 
+site_project.df <- dbGetQuery(con, "SELECT * FROM site_project"); site_project.df
+profile.df <- dbGetQuery(con, "SELECT * FROM profile"); profile.df
+element.df <- dbGetQuery(con, "SELECT * FROM element"); element.df
+unit_of_measure.df <- dbGetQuery(con, "SELECT * FROM unit_of_measure"); unit_of_measure.df
+procedure_phys_chem.df <- dbGetQuery(con, "SELECT * FROM procedure_phys_chem"); procedure_phys_chem.df
+property_phys_chem.df <- dbGetQuery(con, "SELECT * FROM property_phys_chem"); property_phys_chem.df
+observation_phys_chem.df <- dbGetQuery(con, "SELECT * FROM observation_phys_chem"); observation_phys_chem.df
 
-## Table:  profile
-create_table_query <- " CREATE TABLE IF NOT EXISTS profile (
-  profile_id SERIAL PRIMARY KEY,
-  profile_code VARCHAR(255),
-  plot_id INTEGER NOT NULL,
-  FOREIGN KEY (plot_id) REFERENCES plot(plot_id)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  unit_of_measure
-create_table_query <- " CREATE TABLE IF NOT EXISTS unit_of_measure (
-  unit_of_measure_id SERIAL PRIMARY KEY,
-  label VARCHAR(255),
-  description TEXT,
-  url VARCHAR(255)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  property_phys_chem
-create_table_query <- " CREATE TABLE IF NOT EXISTS property_phys_chem (
-  property_phys_chem_id SERIAL PRIMARY KEY,
-  label VARCHAR(255),
-  url VARCHAR(255)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  procedure_phys_chem
-create_table_query <- " CREATE TABLE IF NOT EXISTS procedure_phys_chem (
-  procedure_phys_chem_id SERIAL PRIMARY KEY,
-  label VARCHAR(255),
-  url VARCHAR(255)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  glosis_procedures
-create_table_query <- " CREATE TABLE IF NOT EXISTS glosis_procedures (
-  procedure_id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  description TEXT
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  element
-create_table_query <- " CREATE TABLE IF NOT EXISTS element (
-  element_id SERIAL PRIMARY KEY,
-  type VARCHAR(255),
-  profile_id INTEGER NOT NULL,
-  order_element INTEGER,
-  upper_depth NUMERIC,
-  lower_depth NUMERIC,
-  specimen_id INTEGER,
-  specimen_code VARCHAR(255),
-  FOREIGN KEY (profile_id) REFERENCES profile(profile_id)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  observation_phys_chem
-create_table_query <- " CREATE TABLE IF NOT EXISTS observation_phys_chem (
-  observation_phys_chem_id SERIAL PRIMARY KEY,
-  property_phys_chem_id INTEGER NOT NULL,
-  procedure_phys_chem_id INTEGER NOT NULL,
-  unit_of_measure_id INTEGER NOT NULL,
-  value_min NUMERIC,
-  value_max NUMERIC,
-  observation_phys_chem_r_label VARCHAR(255),
-  FOREIGN KEY (property_phys_chem_id) REFERENCES property_phys_chem(property_phys_chem_id),
-  FOREIGN KEY (procedure_phys_chem_id) REFERENCES procedure_phys_chem(procedure_phys_chem_id),
-  FOREIGN KEY (unit_of_measure_id) REFERENCES unit_of_measure(unit_of_measure_id)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  result_phys_chem
-create_table_query <- " CREATE TABLE IF NOT EXISTS result_phys_chem (
-  result_phys_chem_id SERIAL PRIMARY KEY,
-  observation_phys_chem_id INTEGER NOT NULL,
-  element_id INTEGER NOT NULL,
-  value NUMERIC,
-  FOREIGN KEY (observation_phys_chem_id) REFERENCES observation_phys_chem(observation_phys_chem_id),
-  FOREIGN KEY (element_id) REFERENCES element(element_id)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-## Table:  site_project
-create_table_query <- " CREATE TABLE IF NOT EXISTS site_project (
-  site_id INTEGER NOT NULL,
-  project_id INTEGER NOT NULL,
-  PRIMARY KEY (site_id, project_id),
-  FOREIGN KEY (site_id) REFERENCES site(site_id),
-  FOREIGN KEY (project_id) REFERENCES project(project_id)
-);
-"
-# Execute the SQL command
-dbSendQuery(conn, create_table_query)
-
-### END OF SCRIPT
-
+result_phys_chem.df <- dbGetQuery(con, "SELECT * FROM result_phys_chem"); result_phys_chem.df
+glosis_procedures.df <- dbGetQuery(con, "SELECT * FROM glosis_procedures"); glosis_procedures.df
 
 ### ADD DATA TO TABLES----
+
 ## EXAMPLE WITH A TIBBLE
 site_tibble <- tibble(
   site_id = c(1, 2),
@@ -196,21 +63,24 @@ site_tibble <- tibble(
   project_name = c("ProjectA", "ProjectB")
 )
 
+#site_tibble <- read.csv("site_test.csv")
+site_tibble
+
 # Assuming you have a function to safely execute SQL commands
-safeExecute <- function(conn, query) {
+safeExecute <- function(con, query) {
   tryCatch({
-    dbSendQuery(conn, query)
+    dbSendQuery(con, query)
   }, error = function(e) {
     cat("Error in executing SQL: ", e$message, "\n")
   })
 }
 
-# Insert data into the 'projects' table
+# Insert data into the 'project' table
 unique_projects <- unique(site_tibble[, c("project_id", "project_name")])
 for (row in 1:nrow(unique_projects)) {
   query <- sprintf("INSERT INTO project (project_id, name) VALUES (%d, '%s') ON CONFLICT (project_id) DO NOTHING;",
                    unique_projects$project_id[row], unique_projects$project_name[row])
-  safeExecute(conn, query)
+  safeExecute(con, query)
 }
 
 # Insert data into the 'site' table
@@ -219,16 +89,7 @@ unique_sites <- unique(site_tibble[, c("site_id", "site_code", "longitude", "lat
 for (row in 1:nrow(unique_sites)) {
   query <- sprintf("INSERT INTO site (site_id, site_code, location) VALUES (%d, '%s', ST_SetSRID(ST_MakePoint(%f, %f), 4326)) ON CONFLICT (site_id) DO NOTHING;",
                    unique_sites$site_id[row], unique_sites$site_code[row], unique_sites$longitude[row], unique_sites$latitude[row])
-  safeExecute(conn, query)
-}
-
-# Insert data into the 'site' table
-# Assuming 'position' is generated from 'longitude' and 'latitude', and these fields exist in your 'site' table
-unique_sites <- unique(site_tibble[, c("site_id", "site_code", "longitude", "latitude")])
-for (row in 1:nrow(unique_sites)) {
-  query <- sprintf("INSERT INTO site (site_id, site_code, location) VALUES (%d, '%s', ST_SetSRID(ST_MakePoint(%f, %f), 4326)) ON CONFLICT (site_id) DO NOTHING;",
-                   unique_sites$site_id[row], unique_sites$site_code[row], unique_sites$longitude[row], unique_sites$latitude[row])
-  safeExecute(conn, query)
+  safeExecute(con, query)
 }
 
 # Insert data into the 'plot' table
@@ -241,22 +102,144 @@ for (i in 1:nrow(unique_plots)) {
     row$plot_id, row$plot_code, row$site_id, row$plot_type
   )
   # Execute the SQL command
-  safeExecute(conn, query)
+  safeExecute(con, query)
 }
+
+# Insert data into the 'site_project' table
+unique_sites <- unique(site_tibble[, c("site_id", "project_id")])
+# Insert data
+for (i in 1:nrow(unique_sites)) {
+  # Prepare the SQL INSERT statement
+  query <- sprintf("INSERT INTO site_project (site_id, project_id) VALUES (%s, %s) ON CONFLICT DO NOTHING;",
+                   unique_sites$site_id[i], unique_sites$project_id[i])
+  # Execute the SQL command
+  safeExecute(con, query)
+}
+
+# Insert data into the 'profile' table
+unique_sites <- unique(site_tibble[, c("profile_id", "profile_code", "plot_id")])
+# Insert data
+for (i in 1:nrow(unique_sites)) {
+  query <- sprintf("INSERT INTO profile (profile_id, profile_code, plot_id) VALUES (%d, '%s', %d) ON CONFLICT DO NOTHING;",
+                   unique_sites$profile_id[i], unique_sites$profile_code[i], unique_sites$plot_id[i])
+  safeExecute(con, query)
+}
+
+# Insert data into the 'element' table
+unique_sites <- unique(site_tibble[c("profile_id")])
+for (i in 1:nrow(unique_sites)) {
+  # Prepare the SQL INSERT statement
+  query <- sprintf("INSERT INTO element (profile_id) VALUES (%d) ON CONFLICT (element_id) DO NOTHING;",
+    unique_sites$profile_id[i])
+  # Execute the SQL INSERT statement
+  safeExecute(con, query)
+}
+
+# Insert data into the 'unit_of_measure' table
+# unique_sites <- unique(site_tibble[, c("unit_of_measure_id", "label", "description", "url")])
+# for (i in 1:nrow(unit_of_measure_tibble)) {
+#   # Prepare the SQL INSERT statement
+#   query <- sprintf(
+#     "INSERT INTO unit_of_measure (label, description, url) VALUES ('%s', '%s', '%s') ON CONFLICT (unit_of_measure_id) DO NOTHING;",
+#     unit_of_measure_tibble$label[i], 
+#     unit_of_measure_tibble$description[i], 
+#     unit_of_measure_tibble$url[i]
+#   )
+#   # Execute the SQL INSERT statement
+#   safeExecute(con, query)
+# }
+
+# # Insert data into the 'procedure_phys_chem' table
+# unique_sites <- unique(site_tibble[, c("procedure_phys_chem_id", "label", "url")])
+# for (i in 1:nrow(unique_sites)) {
+#   # Prepare the SQL INSERT statement
+#   query <- sprintf(
+#     "INSERT INTO procedure_phys_chem (procedure_phys_chem_id, label, url) VALUES (%d, '%s', '%s') ON CONFLICT (procedure_phys_chem_id) DO NOTHING;",
+#     unique_sites$procedure_phys_chem_id[i], 
+#     unique_sites$label[i], 
+#     unique_sites$url[i]
+#   )
+#   
+#   # Execute the SQL INSERT statement
+#   safeExecute(con, query)
+# }
+
+# # Insert data into the 'property_phys_chem_id' table
+# unique_sites <- unique(site_tibble[, c("property_phys_chem_id", "label", "url")])
+# for (i in 1:nrow(unique_sites)) {
+#   # Prepare the SQL INSERT statement
+#   query <- sprintf("INSERT INTO property_phys_chem_id (property_phys_chem_id, label, url) VALUES (%d, '%s', '%s') ON CONFLICT (property_phys_chem_id) DO NOTHING;",
+#                    unique_sites$property_phys_chem_id[i], 
+#                    unique_sites$label[i], 
+#                    unique_sites$url[i]
+#   )
+#   
+#   # Execute the SQL INSERT statement
+#   safeExecute(con, query)
+# }
+
+  
+# # Insert data into the 'observation_phys_chem' table
+# unique_sites <- unique(site_tibble[c("observation_phys_chem_id","property_phys_chem_id","procedure_phys_chem_id","unit_of_measure_id","value_min","value_max","observation_phys_chem_r_label")])
+# for (i in 1:nrow(unique_sites)) {
+#   # Prepare the SQL INSERT statement
+#   query <- sprintf(
+#     "INSERT INTO observation_phys_chem (property_phys_chem_id, procedure_phys_chem_id, unit_of_measure_id, value_min, value_max, observation_phys_chem_r_label) VALUES (%d, %d, %d, %d, %d, %d,'%s') ON CONFLICT (observation_phys_chem_id) DO NOTHING;",
+#     unique_sites$property_phys_chem_id[i], 
+#     unique_sites$procedure_phys_chem_id[i],
+#     unique_sites$unit_of_measure_id[i],
+#     unique_sites$value_min[i], 
+#     unique_sites$value_max[i],
+#     unique_sites$observation_phys_chem_r_label[i]
+#   )
+#   # Execute the SQL INSERT statement
+#   safeExecute(con, query)
+# }
+
+# # Insert data into the 'result_phys_chem' table
+# unique_sites <- unique(site_tibble[c("result_phys_chem_id", "observation_phys_chem_id","element_id","value")])
+# for (i in 1:nrow(result_phys_chem_tibble)) {
+#   # Prepare the SQL INSERT statement
+#   query <- sprintf(
+#     "INSERT INTO result_phys_chem (observation_phys_chem_id, element_id, value) VALUES (%d, %d, %f) ON CONFLICT (result_phys_chem_id) DO NOTHING;",
+#     result_phys_chem_tibble$observation_phys_chem_id[i], 
+#     result_phys_chem_tibble$element_id[i],
+#     result_phys_chem_tibble$value[i]
+#   )
+#   
+#   # Execute the SQL INSERT statement
+#   safeExecute(con, query)
+# }
+
+
+
+# # Insert data into the 'glosis_procedures' table
+# unique_sites <- unique(site_tibble[c("result_phys_chem_id", "observation_phys_chem_id","element_id","value")])
+# for (i in 1:nrow(unique_sites)) {
+#   # Prepare the SQL INSERT statement
+#   query <- sprintf(
+#     "INSERT INTO glosis_procedures (name, description) VALUES ('%s', '%s', ) ON CONFLICT (procedure_id) DO NOTHING;",
+#     unique_sites$name[i], 
+#     unique_sites$description[i]
+#   )
+#   # Execute the SQL INSERT statement
+#   safeExecute(con, query)
+# }
+
+
+
+# Fetch data
+df <- dbGetQuery(con, "SELECT * FROM site"); df # Los datos los he añadido con el tibble csv
+df <- dbGetQuery(con, "SELECT * FROM plot"); df
+df <- dbGetQuery(con, "SELECT * FROM project"); df
+df <- dbGetQuery(con, "SELECT * FROM site_project"); df
 
 
 # Close the connection
-dbDisconnect(conn)
+dbDisconnect(con)
 
 
 ### END OF SCRIPT
-# Create databases in psql
-
-# \list
-# \dt
-# 
-# CREATE DATABASE carsis;
-
 
 
 
